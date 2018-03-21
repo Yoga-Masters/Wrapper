a757a541ca96571b66f45e62d812f14a5e26d666
AdamTheHousman <adamhousman@gmail.com>
2018-03-20 17:03:57 -0700

    TesterApp

---
+++ ExpressApp1/routes/index.js
@@ -0,0 +1,16 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    res.render('index', { title: 'Express' });
+});
+
+router.get('/api', function (req, res) {
+	res.send({
+		"key": console.log(require('./build/Release/native.node').hello())
+	})
+})
+
+module.exports = router;
